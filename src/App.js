96f303f3c28f9603cde20342dd4556193175a268
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-08 23:38:15 +0600

    firebase deploy done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import {router} from './Router/Routes/Routes';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
